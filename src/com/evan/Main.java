012b6ae96947616f5b6e0ede9ae07d8d6f79c6ee
Evan <evan.sorger@gmail.com>
2021-03-07 20:13:28 -0500

    Adding queue implmentation

--- src/com/evan/Main.java
+++ src/com/evan/Main.java
@@ -11,7 +11,6 @@ public class Main {
         stack.push(3);
         stack.push(4);
         stack.push(5);
-
         System.out.println("Current top of stack: " + stack.peek());
         int pop = stack.pop();
         System.out.println("Popped value: " + pop);
@@ -19,5 +18,22 @@ public class Main {
         System.out.println("Pushing 10");
         stack.push(10);
         System.out.println("Current top of stack: " + stack.peek());
+
+        System.out.println("\n***QUEUE EXAMPLE***");
+        System.out.println("Initializing new queue of size 4");
+        Queue queue = new Queue(4);
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.printQueue();
+        System.out.println("Dequeue one element");
+        queue.dequeue();
+        queue.printQueue();
+        System.out.println("Enqueue one element");
+        queue.enqueue(1);
+        queue.printQueue();
+
+        System.out.println("\n***LINKED LIST EXAMPLE***");
     }
 }
--- src/com/evan/Queue.java
+++ src/com/evan/Queue.java
@@ -1,4 +1,45 @@
 package com.evan;
 
+import java.util.Arrays;
+
 public class Queue {
+    private int size;
+    private int first = 0;
+    private int last = 0;
+    private int[] array;
+
+    public Queue(int size) {
+        this.size = size;
+        array = new int[size];
+    }
+
+    public void enqueue(int i) {
+        if (last == size) {
+            System.out.println("Queue is full");
+        } else {
+            array[last] = i;
+            last++;
+        }
+    }
+
+    public void dequeue() {
+        if (last == first) {
+            System.out.println("Queue is empty");
+        } else {
+            for (int i = 0; i < last - 1; i++) {
+                array[i] = array[i + 1];
+            }
+            array[last-1] = 0;
+            last--;
+        }
+    }
+
+    public int top() {
+        return array[first];
+    }
+
+    public void printQueue() {
+        Arrays.stream(array).asLongStream().forEach(System.out::print);
+        System.out.println();
+    }
 }
